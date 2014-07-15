64b7978897af2f2c492316601defa06400993f05
Dror Helper <dror.helper@gmail.com>
2014-07-15 22:07:42 +0300

    First failing test

---
+++ PhoneCall/PhoneCall.Core.Tests/PhoneTests.cs
@@ -0,0 +1,29 @@
+﻿using FakeItEasy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneCall.Core.Tests
+{
+    [TestFixture]
+    public class PhoneTests
+    {
+        [Test]
+        public void OnPhoneRings_PhoneRingsMessageRecieved_PhoneRingEventRaised()
+        {
+            var fakeClient = A.Fake<IPhoneClient>();
+
+            var wasCalled = false;
+
+            var phone = new Phone(fakeClient);
+            phone.OnRing += (o, e) => wasCalled = true;
+
+            fakeClient.OnPhoneRing += Raise.WithEmpty().Now;
+
+            Assert.That(fakeClient, Is.True);
+        }
+    }
+}
---
+++ PhoneCall/PhoneCall.Core/Phone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneCall.Core
+{
+    public class Phone
+    {
+        private IPhoneClient _client;
+
+        public event EventHandler OnRing;
+
+        public Phone(IPhoneClient client)
+        {
+            this._client = _client;
+        }
+    }
+
+}
