3ba850f45d2cde2e96c8e61f1bafff9807f6489c
Dror Helper <dror.helper@gmail.com>
2015-01-25 21:32:51 +0200

    Added a 2nd test for new requirments

--- PhoneCall/PhoneCall.Core.Tests/PhoneTests.cs
+++ PhoneCall/PhoneCall.Core.Tests/PhoneTests.cs
@@ -23,6 +23,20 @@ namespace PhoneCall.Core.Tests
         }
 
         [Test]
+        public void OnPhoneRings_PhoneRingsMessageAndUserAcceptsCall_DoNotConnectToServer()
+        {
+            var fakeClient = A.Fake<IPhoneClient>();
+
+            var phone = new Phone(fakeClient);
+
+            fakeClient.OnPhoneRing += Raise.WithEmpty().Now;
+
+            phone.AcceptCall();
+
+            A.CallTo(() => fakeClient.Connect()).MustNotHaveHappened();
+        }
+
+        [Test]
         public void AcceptCall_WithoutPhoneRing_DoNotConnectToServer()
         {
             var fakeClient = A.Fake<IPhoneClient>();
