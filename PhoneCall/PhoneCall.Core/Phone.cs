6144db3018a7495b51b2c7a17fef6ede40642d0f
Dror Helper <dror.helper@gmail.com>
2015-01-25 21:35:11 +0200

    Added code to make tests pass

--- PhoneCall/PhoneCall.Core/Phone.cs
+++ PhoneCall/PhoneCall.Core/Phone.cs
@@ -6,11 +6,21 @@ namespace PhoneCall.Core
     {
         private readonly IPhoneClient _client;
         private bool _phoneAlreadyRang;
+        private bool _callAccepted;
 
         public Phone(IPhoneClient client)
         {
             _client = client;
             _client.OnPhoneRing += HandlePhoneRing;
+            _client.OnConnectedToServer += HandleConnectedToServer;
+        }
+
+        private void HandleConnectedToServer(object sender, EventArgs e)
+        {
+            if (_callAccepted)
+            {
+                _client.Connect();
+            }
         }
 
         private void HandlePhoneRing(object sender, EventArgs e)
@@ -22,7 +32,7 @@ namespace PhoneCall.Core
         {
             if (_phoneAlreadyRang)
             {
-                _client.Connect();
+                _callAccepted = true;
             }
         }
 
